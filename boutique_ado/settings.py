9c176e0bdbc97a5d43924394c79c03b46a36e667
Kieran Collins <kiecol@protonmail.com>
2023-06-06 12:53:25 +0000

    Add shopping bag context and delivery logic

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -76,6 +76,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',  # required by allauth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'bag.contexts.bag_contents',  # items in shopping bag
             ],
         },
     },
@@ -161,3 +162,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Custom Fields (Delivery)
+FREE_DELIVERY_THRESHOLD = 50
+STANDARD_DELIVERY_PERCENTAGE = 10
