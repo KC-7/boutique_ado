4633db39875ed7862d16b0991e61492bc3d8ec0c
Kieran Collins <kiecol@protonmail.com>
2023-06-07 17:55:04 +0000

    Set up Stripe keys (BUG)

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -175,6 +175,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# Custom Fields (Delivery)
+# Stripe & Delivery
 FREE_DELIVERY_THRESHOLD = 50
 STANDARD_DELIVERY_PERCENTAGE = 10
+STRIPE_CURRENCY = 'usd'
+STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
+STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
