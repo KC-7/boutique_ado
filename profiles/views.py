f1f37230cfdce0630fca3e7e873670951e3daba9
Kieran Collins <kiecol@protonmail.com>
2023-06-20 15:02:13 +0000

    Product Admin - Secure the views

--- profiles/views.py
+++ profiles/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
 from .models import UserProfile
 from .forms import UserProfileForm
 
 from checkout.models import Order
 
+
+@login_required
 def profile(request):
     """ Display the user's profile. """
     profile = get_object_or_404(UserProfile, user=request.user)
@@ -15,7 +18,7 @@ def profile(request):
         if form.is_valid():
             form.save()
             messages.success(request, 'Profile updated successfully')
-        else: 
+        else:
             messages.error(request, 'Update failed. Please ensure the form is valid.')
     else:
         form = UserProfileForm(instance=profile)
